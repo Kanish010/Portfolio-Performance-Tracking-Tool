21c2377bffcd776aba98ad5c7a29f7475f7dcd62
Kanish010 <kanish.alluri@gmail.com>
2024-01-16 15:10:43 -0800

    init commit

--- Portfolio Optimization/main.py
+++ Portfolio Optimization/main.py
@@ -1,6 +1,6 @@
 from portfolio_optimizer import PortfolioOptimizer
 from portfolio_gui import PortfolioGUI
-#fg
+
 if __name__ == "__main__":
     optimizer = PortfolioOptimizer()
     gui = PortfolioGUI(optimizer)
