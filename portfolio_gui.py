f3df55d3cfcf0197e20f6fc7a5c93c9d688b08cb
Kanish010 <kanish.alluri@gmail.com>
2024-01-17 14:04:31 -0800

    init commit

--- portfolio_gui.py
+++ portfolio_gui.py
@@ -115,7 +115,7 @@ class PortfolioGUI:
                 if not stock:
                     break
 
-                stock = stock.upper().rstrip()
+                stock = stock.upper().replace(" ", "")
                 historical_data = self.nn_optimizer.historical_stock_data(stock)
                 if not historical_data.empty:
                     historical_data_list.append((stock, historical_data))
