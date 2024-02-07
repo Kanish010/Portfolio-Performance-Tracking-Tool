4794ab597467f4e9e7fbaf46971772cac54923bd
Kanish010 <kanish.alluri@gmail.com>
2024-02-07 14:41:32 -0800

    init commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from neural_net_optimizer import NeuralNetOptimizer
 from monte_carlo_optimizer import MonteCarloOptimizer
+from quantum_comp_optimizer import QuantumAnnealingOptimizer
 from portfolio_gui import PortfolioGUI
 
 if __name__ == "__main__":
     nn_optimizer = NeuralNetOptimizer()
     mc_optimizer = MonteCarloOptimizer()
-    gui = PortfolioGUI(nn_optimizer, mc_optimizer)
+    qa_optimizer = QuantumAnnealingOptimizer() 
+    gui = PortfolioGUI(nn_optimizer, mc_optimizer, qa_optimizer)
     gui.run_gui()
