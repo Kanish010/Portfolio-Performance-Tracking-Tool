f6817156136e5a58c4188a4a84c182c11ada3d09
Kanish010 <kanish.alluri@gmail.com>
2024-03-19 12:02:18 -0700

    init commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from neural_net_optimizer import NeuralNetOptimizer
 from monte_carlo_optimizer import MonteCarloOptimizer
 from quantum_comp_optimizer import QuantumAnnealingOptimizer
+from SQLConnector import DatabaseManager
 from portfolio_gui import PortfolioGUI
 
 if __name__ == "__main__":
     nn_optimizer = NeuralNetOptimizer()
     mc_optimizer = MonteCarloOptimizer()
     qa_optimizer = QuantumAnnealingOptimizer() 
+    db_manager = DatabaseManager()
     gui = PortfolioGUI(nn_optimizer, mc_optimizer, qa_optimizer)
     gui.run_gui()
